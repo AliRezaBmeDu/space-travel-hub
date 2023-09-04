c9e6a91ed181627b528e7851adff525a71b11c9c
unknown <albertkantwi@gmail.com>
2023-09-04 14:29:49 +0100

    Add Navbar to project

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import './App.css';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       Hello, This is the space-traveller&lsquo;s-hub
     </div>
   );
