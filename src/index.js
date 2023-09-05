dac5f3f9ea54d80ed650f0ff6cb5845748323628
AliReza <ali.reza.buet@gmail.com>
2023-09-05 15:22:05 +0600

    Add Provider

--- src/index.js
+++ src/index.js
@@ -3,7 +3,8 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-// import store from './redux/store';
+import store from './redux/store';
+import { Provider } from 'react-redux';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
