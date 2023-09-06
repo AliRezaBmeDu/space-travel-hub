0b013ebaff12f9413c884a494b9815b612b635d9
AliReza <ali.reza.buet@gmail.com>
2023-09-07 04:16:34 +0600

    Update the profile rocket style. Fix the linters

--- src/components/ProfileRocket.jsx
+++ src/components/ProfileRocket.jsx
@@ -1,17 +1,29 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
+import '../css/ProfileRocket.css';
 
 const ProfileRocket = () => {
   const { rockets } = useSelector((store) => store.rockets);
   console.log('rockets', rockets);
 
-  if (!rockets) {
-    return <div>Loading...</div>;
+  const reservedRockets = rockets.filter((rocket) => rocket.reserve === true);
+
+  console.log('reserved rockets', reservedRockets);
+
+  if (reservedRockets.length === 0) {
+    return (
+      <div className="reserved-container">
+        <h2>Reserved Rockets</h2>
+        <div className="rocket-profile">
+          <p>None</p>
+        </div>
+      </div>
+    );
   }
 
-  const reservedRockets = rockets.filter((rocket) => rocket.reserve === true);
   return (
-    <div>
+    <div className="reserved-container">
+      <h2>Reserved Rockets</h2>
       {reservedRockets && reservedRockets.map((rocket) => (
         <div key={rocket.rocketId} className="rocket-profile">
           <p>{rocket.rocketName}</p>
