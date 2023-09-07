b69b78aa1f76d5408eeb0082f03bdfa5c4dc0c8b
AliReza <ali.reza.buet@gmail.com>
2023-09-07 21:36:29 +0600

    Fix linter error

--- src/redux/rockets/rocketsSlice.js
+++ src/redux/rockets/rocketsSlice.js
@@ -11,6 +11,7 @@ export const getRockets = createAsyncThunk('rockets/getRocket', async () => {
     const output = response.json();
     return output;
   } catch (error) {
+    console.error('Error fetching data');
     throw error;
   }
 });
